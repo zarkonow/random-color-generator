f1f3598b755fd19a54444b0c86bf4ad2019efe1a
zarkonow <stojanov1987@gmail.com>
2024-10-18 19:44:52 +0200

    *added buttons

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 
 
 
-export default function RandomColor () {
 
+export default function RandomColor() {
+const [typeOfColor, setTypeOfColor] = useState('hex')
+const [color, setColor] = useState('#000000')
 
-    return (
-        <div className="container">
-            <button>Generate Random Color</button>
-        </div>
-    )
+function handleCreateRandomHexColor() {
+
+}
+
+function handleCreateRandomRgbColor() {
+
+}
+
+  return (
+    <div style={{
+      width: '100%',
+      height: '100%',
+      backgroundColor: color,
+    }}>
+      <button onClick={() => setTypeOfColor('hex')}>Generate HEX Color</button>
+      <button onClick={() => setColor('rgb')}>Generate RGB Color</button>
+      <button onClick={typeOfColor === 'hex' ? handleCreateRandomHexColor : handleCreateRandomRgbColor}>Generate Random Color</button>
+    </div>
+  );
 }
